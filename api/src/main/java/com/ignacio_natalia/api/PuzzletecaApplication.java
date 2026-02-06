ff79db8ce29a5016ba127b4819b8b481280af00d
Isanz905 <ignaciosanz511@gmail.com>
2026-02-06 16:25:10 +0100

    Initial commit - Spring Boot API setup

---
+++ api/src/main/java/com/ignacio_natalia/api/PuzzletecaApplication.java
@@ -0,0 +1,13 @@
+package com.ignacio_natalia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzletecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzletecaApplication.class, args);
+	}
+
+}
