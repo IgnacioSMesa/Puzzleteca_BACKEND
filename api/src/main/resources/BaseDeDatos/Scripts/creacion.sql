f59e455968019fb5dde0a60508a1b9c08beebe90
Natalia242 <nataliavkhanacademy@gmail.com>
2026-04-23 20:37:03 +0200

    Nueva tabla posts del foro

--- api/src/main/resources/BaseDeDatos/Scripts/creacion.sql
+++ api/src/main/resources/BaseDeDatos/Scripts/creacion.sql
@@ -5,11 +5,12 @@ SET search_path TO puzzles;
 -- ==========================
 -- TABLA USUARIO
 -- ==========================
-CREATE TABLE usuario (
-    id_usuario INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
+CREATE TABLE usuario
+(
+    id_usuario  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nombre      VARCHAR(100) NOT NULL,
+    apellido    VARCHAR(100) NOT NULL,
+    email       VARCHAR(150) NOT NULL UNIQUE,
     contrasenna VARCHAR(255) NOT NULL,
     tipousuario VARCHAR(50) DEFAULT 'Usuario'
 );
@@ -17,51 +18,54 @@ CREATE TABLE usuario (
 -- ==========================
 -- TABLA PUZZLE
 -- ==========================
-CREATE TABLE puzzle (
-    id_puzzle INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    titulo VARCHAR (150),
-    autor VARCHAR(150),
-    tiempo INT,
-    piezas INT,
-    dificultad VARCHAR(50),
+CREATE TABLE puzzle
+(
+    id_puzzle   INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    titulo      VARCHAR(150),
+    autor       VARCHAR(150),
+    tiempo      INT,
+    piezas      INT,
+    dificultad  VARCHAR(50),
     descripcion TEXT,
-    color BOOLEAN,
-    valoracion INT,
-    estado text,
-    id_usuario INT NOT NULL,
-    imagen_url TEXT DEFAULT NULL,
+    color       BOOLEAN,
+    valoracion  INT  DEFAULT 0 CHECK (valoracion >= 0),
+    estado      text,
+    imagen_url  TEXT DEFAULT NULL,
+    id_usuario  INT NOT NULL,
 
     CONSTRAINT fk_puzzle_usuario
         FOREIGN KEY (id_usuario)
-            REFERENCES usuario(id_usuario)
+            REFERENCES usuario (id_usuario)
             ON DELETE CASCADE
 );
 
 -- ==========================
 -- TABLA CONVERSACION
 -- ==========================
-CREATE TABLE conversacion (
+CREATE TABLE conversacion
+(
     id_conversation INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    creado_en TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    actualizado_en TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    creado_en       TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    actualizado_en  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 -- ==========================
 -- TABLA PARTICIPANTES_CONVERSACION
 -- ==========================
-CREATE TABLE participantes_conversacion (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE participantes_conversacion
+(
+    id              INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     id_conversation INT NOT NULL,
-    id_usuario INT NOT NULL,
+    id_usuario      INT NOT NULL,
 
     CONSTRAINT fk_participante_conversacion
         FOREIGN KEY (id_conversation)
-            REFERENCES conversacion(id_conversation)
+            REFERENCES conversacion (id_conversation)
             ON DELETE CASCADE,
 
     CONSTRAINT fk_participante_usuario
         FOREIGN KEY (id_usuario)
-            REFERENCES usuario(id_usuario)
+            REFERENCES usuario (id_usuario)
             ON DELETE CASCADE,
 
     CONSTRAINT unique_participante
@@ -71,39 +75,67 @@ CREATE TABLE participantes_conversacion (
 -- ==========================
 -- TABLA MENSAJE
 -- ==========================
-CREATE TABLE mensaje (
-    id_mensaje INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    contenido TEXT NOT NULL,
-    creado_en TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    leido_en TIMESTAMPTZ NULL,
-    id_conversation INT NOT NULL,
-    id_usuario INT NOT NULL,
+CREATE TABLE mensaje
+(
+    id_mensaje      INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    contenido       TEXT NOT NULL,
+    creado_en       TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    leido_en        TIMESTAMPTZ DEFAULT NULL,
+    id_conversation INT  NOT NULL,
+    id_usuario      INT  NOT NULL,
 
     CONSTRAINT fk_mensaje_conversacion
-     FOREIGN KEY (id_conversation)
-         REFERENCES conversacion(id_conversation)
-         ON DELETE CASCADE,
+        FOREIGN KEY (id_conversation)
+            REFERENCES conversacion (id_conversation)
+            ON DELETE CASCADE,
 
     CONSTRAINT fk_mensaje_usuario
-     FOREIGN KEY (id_usuario)
-         REFERENCES usuario(id_usuario)
-         ON DELETE CASCADE
+        FOREIGN KEY (id_usuario)
+            REFERENCES usuario (id_usuario)
+            ON DELETE CASCADE
 );
-CREATE TABLE comentario (
-    id_comentario INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    contenido TEXT NOT NULL,
-    creado_en TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 
-    id_usuario INT NOT NULL,
-    id_puzzle INT NOT NULL,
+-- ==========================
+-- TABLA POST (foro)
+-- ==========================
+CREATE TABLE post
+(
+    id_post           INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    contenido         TEXT,
+    imagen_url        TEXT,
+
+    fecha_creacion    TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+    total_likes       INT         DEFAULT 0 CHECK (total_likes >= 0),
+    total_comentarios INT         DEFAULT 0 CHECK (total_comentarios >= 0),
+
+    id_usuario        INT NOT NULL,
+
+    CONSTRAINT fk_post_usuario
+        FOREIGN KEY (id_usuario)
+            REFERENCES usuario (id_usuario)
+            ON DELETE CASCADE
+);
+
+-- ==========================
+-- TABLA COMENTARIO (Comentarios de un post)
+-- ==========================
+CREATE TABLE comentario
+(
+    id_comentario  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    contenido      TEXT NOT NULL,
+    fecha_creacion TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+    id_usuario     INT  NOT NULL,
+    id_post        INT  NOT NULL,
 
     CONSTRAINT fk_comentario_usuario
         FOREIGN KEY (id_usuario)
-            REFERENCES usuario(id_usuario)
+            REFERENCES usuario (id_usuario)
             ON DELETE CASCADE,
 
-    CONSTRAINT fk_comentario_puzzle
-        FOREIGN KEY (id_puzzle)
-            REFERENCES puzzle(id_puzzle)
+    CONSTRAINT fk_comentario_post
+        FOREIGN KEY (id_post)
+            REFERENCES post (id_post)
             ON DELETE CASCADE
 );
